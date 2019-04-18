19aecd4553a2a75b78c5981f8f60cc8da298efef
Andrew Vande Guchte <avandegu@umich.edu>
2019-04-17 21:56:44 -0400

    Added sticky h1 bars

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,20 @@ $( document ).ready(function() {
     }
   });
 
+// Scroll-lock for h1
 
+var stickyh1_from_top = $('.stick-lock').offset().top;
+
+$( window ).scroll(function() {
+  var scroll_from_top = $(window).scrollTop();
+  if (scroll_from_top > stickyh1_from_top) {
+    $('.stick-lock').addClass('sticky')
+    $('#content').addClass('sticky-content')
+  } else {
+    $('.stick-lock').removeClass('sticky')
+    $('#content').removeClass('sticky-content')
+  }
+});
 
 
 
